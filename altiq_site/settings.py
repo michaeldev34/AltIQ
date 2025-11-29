b850df89aacf2ea398413f168d023266d1a3d0b1
Miguel Camacho <miguelcamacho@Miguels-MacBook-Air.local>
2025-11-29 16:06:24 -0600

    Remove Google social login and simplify auth UI

--- altiq_site/settings.py
+++ altiq_site/settings.py
@@ -24,8 +24,6 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     "allauth",
     "allauth.account",
-    "allauth.socialaccount",
-    "allauth.socialaccount.providers.google",
     "core",
     "services",
     "cases",
